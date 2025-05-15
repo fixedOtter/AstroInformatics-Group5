4c15e346e845d79a357be8d640bcaa2b3d10acd7
Mae day <kindquasar@gmail.com>
2025-05-15 13:07:38 -0700

    sigma psf

--- potential_SSR.py
+++ potential_SSR.py
@@ -1,18 +1,18 @@
 from pymongo import MongoClient
 
-uri = "mongodb://group5:IelC3eVkLz%2BMfPlGAKel4g%3D%3D@cmp4818.computers.nau.edu:27018"
-client = MongoClient(uri)
+# uri = "mongodb://group5:IelC3eVkLz%2BMfPlGAKel4g%3D%3D@cmp4818.computers.nau.edu:27018"
+# client = MongoClient(uri)
 
 #select the database
-db = client["ztf"]
+# db = client["ztf"]
 
-#select the collection
-collection = db["snapshot 2"]
+# select the collection
+# collection = db["snapshot 2"]
 
 
 
 def check_for_SSR(output_array):
-    
+    #Empty container for adding 
     Potential_SSR= []
     
     for item in output_array: 
@@ -23,11 +23,14 @@ def check_for_SSR(output_array):
         if  1000 <= period <= 5000: 
             Potential_SSR.append(output_array)
     print(Potential_SSR)
+    
     return Potential_SSR
+
 
                   
 
 
-check_for_SSR()
+check_for_SSR(output_array)
+
 
     
